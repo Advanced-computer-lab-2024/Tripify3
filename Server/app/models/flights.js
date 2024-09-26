ecbc81fcf9dd7dd08b4d1739122525538100f2b1
basilmohamed22 <161706006+basilmohamed22@users.noreply.github.com>
2024-09-26 20:22:17 +0300

    Updated user and flight models

--- Server/app/models/flights.js
+++ Server/app/models/flights.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const filghtSchema = new Schema({
@@ -21,4 +21,4 @@ const filghtSchema = new Schema({
 }, { timestamps: true });
 
 const flights = mongoose.model('flights', userSchema);
-module.exports = flights;
+export default flights
