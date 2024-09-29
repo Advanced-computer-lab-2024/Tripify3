e20bddd046592d51096ab60217a5892644adfd9e
AhmedBackar1 <ahmedhatim226@gmail.com>
2024-09-29 17:06:14 +0300

    admin adding tags and categories

--- Server/src/validation/admin.auth.validation.js
+++ Server/src/validation/admin.auth.validation.js
@@ -15,3 +15,22 @@ export const createschema = Joi.object({
       "string.pattern.base": "Password must contain at least one capital letter, one small letter, one special character, and one number.",
     }),
 });
+export const adminManipulateSchema = Joi.object({
+  name: Joi.string()
+  .required()
+  .messages({
+    "string.pattern.base": "Name must be unique and must not be empty",
+  }),
+});
+export const adminUpdateSchema = Joi.object({
+  oldName: Joi.string()
+  .required()
+  .messages({
+    "string.pattern.base": "Name must be unique and must not be empty",
+  }),
+  newName: Joi.string() 
+  .required()
+  .messages({
+    "string.pattern.base": "Name must be unique and must not be empty",
+  }),
+});
