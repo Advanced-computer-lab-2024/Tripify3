74570c81679d9197b0b0554c240c0368fd910711
Samir ElBatal <samirelbatal0@gmail.com>
2024-09-29 18:50:23 +0300

    removing errors

--- Server/src/routes/admin.user.route.js
+++ Server/src/routes/admin.user.route.js
@@ -1,21 +1,18 @@
-import express from "express";
-import {addTourismGovern,addAdmin,addCategory,viewCategory,updateCategory,deleteCategory,addTag,deleteTag,viewTag,updateTag} from "../controllers/admin.user.controller.js";
-import { validate } from "../middlewares/validation.middleware.js";
-import { createschema,adminManipulateSchema,adminUpdateSchema } from "../validation/admin.auth.validation.js";
-const router = express.Router();
+// import express from "express";
+// import {addTourismGovern,addAdmin,addCategory,viewCategory,updateCategory,deleteCategory,addTag,deleteTag,viewTag,updateTag} from "../controllers/admin.user.controller.js";
+// import { validate } from "../middlewares/validation.middleware.js";
+// import { createschema,adminManipulateSchema,adminUpdateSchema } from "../validation/admin.auth.validation.js";
+// const router = express.Router();
 
-router.post("/access/admin/addTourismGovern", validate(createschema, "body"), addTourismGovern);
-router.post("/access/admin/addAdmin", validate(createschema, "body"),  addAdmin);
-router.post("/access/admin/addCategory", validate(adminManipulateSchema, "body"),  addCategory);
-router.get("/access/admin/viewCategory", viewCategory);
-router.put("/access/admin/updateCategory",validate(adminUpdateSchema, "body"),updateCategory);
-router.delete("/access/admin/deleteCategory", validate(adminManipulateSchema, "body"),  deleteCategory);
-router.post("/access/admin/addTag", validate(adminManipulateSchema, "body"),  addTag);
-router.get("/access/admin/viewTags", viewTag);
-router.put("/access/admin/updateTag",validate(adminUpdateSchema, "body"),updateTag);
-router.delete("/access/admin/deleteTag", validate(adminManipulateSchema, "body"),  deleteTag);
+// router.post("/access/admin/addTourismGovern", validate(createschema, "body"), addTourismGovern);
+// router.post("/access/admin/addAdmin", validate(createschema, "body"),  addAdmin);
+// router.post("/access/admin/addCategory", validate(adminManipulateSchema, "body"),  addCategory);
+// router.get("/access/admin/viewCategory", viewCategory);
+// router.put("/access/admin/updateCategory",validate(adminUpdateSchema, "body"),updateCategory);
+// router.delete("/access/admin/deleteCategory", validate(adminManipulateSchema, "body"),  deleteCategory);
+// router.post("/access/admin/addTag", validate(adminManipulateSchema, "body"),  addTag);
+// router.get("/access/admin/viewTags", viewTag);
+// router.put("/access/admin/updateTag",validate(adminUpdateSchema, "body"),updateTag);
+// router.delete("/access/admin/deleteTag", validate(adminManipulateSchema, "body"),  deleteTag);
 
-
-
-
-export default router;
+// export default router;
--- Server/src/routes/routes.js
+++ Server/src/routes/routes.js
@@ -1,7 +1,7 @@
 import userRoute from "./users.auth.route.js"; // Default import
-import adminroute from "./admin.user.route.js";
+// import adminroute from "./admin.user.route.js";
 
 export default (app) => {
   app.use(userRoute);
-  app.use(adminroute);
+  // app.use(adminroute);
 };
