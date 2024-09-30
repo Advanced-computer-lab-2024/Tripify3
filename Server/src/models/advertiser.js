14c0e8ee9a600d01ddf4bb9da0abb94c175b980d
Samir ElBatal <samirelbatal0@gmail.com>
2024-09-30 17:22:50 +0300

    removing ids

--- Server/src/models/advertiser.js
+++ Server/src/models/advertiser.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const User = require('./User');
 
 const advertiserSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },  // Reference to User
   companyName: { type: String, required: true },
   adBudget: { type: Number, required: true },
   description: { type: String },
--- Server/src/models/seller.js
+++ Server/src/models/seller.js
@@ -3,7 +3,6 @@ const User = require('./User');
 
 
 const sellerSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },  // Reference to User
   companyName: { type: String, required: true },
   sellerID: { type: Number, required: true },
   sellerTaxCard: { type: String, required: true },
