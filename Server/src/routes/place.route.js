ac40cdfd733cd21f997f843549e9c995d57db805
Samir ElBatal <samirelbatal0@gmail.com>
2024-10-09 20:45:03 +0300

    add

--- Server/src/routes/place.route.js
+++ Server/src/routes/place.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {filterPlacesByTags , getPlaces} from '../controllers/location/place.controller.js';
+import {filterPlacesByTags , getPlaces} from '../controllers/place/place.controller.js';
 const router = express.Router();
 
 router.get('/places/filter', filterPlacesByTags);
