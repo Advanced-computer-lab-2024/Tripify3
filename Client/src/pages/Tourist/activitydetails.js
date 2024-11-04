7c102ab55f1f53470cdbca107d8c06256d0d0364
basilmohamed22 <161706006+basilmohamed22@users.noreply.github.com>
2024-11-04 23:19:44 +0200

    small changes in button style

--- Client/src/pages/Tourist/activitydetails.js
+++ Client/src/pages/Tourist/activitydetails.js
@@ -23,6 +23,7 @@ const ActivityDetails = () => {
     };
     fetchActivity();
   }, [id]);
+
   const toggleShareDropdown = (activityId) => {
     // Toggle the dropdown: if the same activity is clicked, close it; otherwise, open it
     if (currentActivityId === activityId) {
@@ -39,6 +40,7 @@ const ActivityDetails = () => {
       setCurrentActivityId(null); // Close dropdown after copying
     });
   };
+
   if (loading) {
     return (
       <Box sx={{ display: 'flex', justifyContent: 'center', mt: 5 }}>
@@ -65,46 +67,60 @@ const ActivityDetails = () => {
       <Typography variant="body1">
         <strong>Description:</strong> {activity.description}
       </Typography>
-      <Button variant="contained" color="primary" href="/tourist/activities">
+      {/* Modified styles for the Back to Activities button */}
+      <Button variant="contained" color="primary" href="/tourist/activities" sx={{ mt: 2 }}>
         Back to Activities
       </Button>
+      {/* Share button with modified styles */}
       <Button
-                    variant="outlined"
-                    sx={{ mt: 2, ml: 2 }}
-                    onClick={() => toggleShareDropdown(activity._id)}
-                  >
-                    Share
-                  </Button>
-                  {currentActivityId === activity._id && (
-                    <Box
-                      sx={{
-                        position: "absolute",
-                        background: "white",
-                        boxShadow: 1,
-                        p: 1,
-                        mt: 1,
-                        zIndex: 10, // Ensure the dropdown appears above other elements
-                      }}
-                    >
-                      <Button variant="text" onClick={() => handleCopyLink(activity._id)}>
-                        Copy Link
-                      </Button>
-                      <Button
-                        variant="text"
-                        href={`https://twitter.com/intent/tweet?url=http://localhost:3000/Tourist/activities/${activity._id}`}
-                        target="_blank"
-                      >
-                        Share on Twitter
-                      </Button>
-                      <Button
-                        variant="text"
-                        href={`https://www.facebook.com/sharer/sharer.php?u=http://localhost:3000/Tourist/activities/${activity._id}`}
-                        target="_blank"
-                      >
-                        Share on Facebook
-                      </Button>
-                    </Box>
-                  )}
-    </Box>)}
+        variant="outlined"
+        sx={{
+          mt: 2,
+          ml: 2,
+          color: 'blue', // Text color
+          borderColor: 'blue', // Border color
+          '&:hover': {
+            backgroundColor: 'lightblue', // Background color on hover
+            color: 'white', // Text color on hover
+          },
+        }}
+        onClick={() => toggleShareDropdown(activity._id)}
+      >
+        Share
+      </Button>
+      
+      {currentActivityId === activity._id && (
+        <Box
+          sx={{
+            position: "absolute",
+            background: "white",
+            boxShadow: 1,
+            p: 1,
+            mt: 1,
+            zIndex: 10, // Ensure the dropdown appears above other elements
+          }}
+        >
+          <Button variant="text" onClick={() => handleCopyLink(activity._id)}>
+            Copy Link
+          </Button>
+          <Button
+            variant="text"
+            href={`https://twitter.com/intent/tweet?url=http://localhost:3000/Tourist/activities/${activity._id}`}
+            target="_blank"
+          >
+            Share on Twitter
+          </Button>
+          <Button
+            variant="text"
+            href={`https://www.facebook.com/sharer/sharer.php?u=http://localhost:3000/Tourist/activities/${activity._id}`}
+            target="_blank"
+          >
+            Share on Facebook
+          </Button>
+        </Box>
+      )}
+    </Box>
+  );
+};
 
 export default ActivityDetails;
